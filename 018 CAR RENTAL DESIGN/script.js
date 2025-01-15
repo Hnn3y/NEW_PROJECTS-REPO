d3ab50f764456aa3e0b13cfd7ab6fb667451421c
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-01-15 16:03:10 +0100

    018

---
+++ 018 CAR RENTAL DESIGN/script.js
@@ -0,0 +1,7 @@
+// Example: Smooth scroll for buttons
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', () => {
+      alert('Button Clicked!');
+    });
+  });
+  
