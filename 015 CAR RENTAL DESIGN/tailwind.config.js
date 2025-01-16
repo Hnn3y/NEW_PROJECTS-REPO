1aca5d8276e7f5a9ff4d859970d36af6a2276819
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-01-16 19:26:07 +0100

    015

---
+++ 015 CAR RENTAL DESIGN/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust this based on your file structure
+    "./*.html", // Include files in the root directory if applicable
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
