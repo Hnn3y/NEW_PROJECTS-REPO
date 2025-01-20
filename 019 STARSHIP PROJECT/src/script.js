0bf51c71a71ff451becce274e2e36bef999ab97d
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-01-20 16:27:31 +0100

    019

--- 019 STARSHIP PROJECT/src/script.js
+++
@@ -1,8 +0,0 @@
-import { gsap } from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-document.addEventListener('DOMContentLoaded', () => {
-  const card = document.getElementById('card');
-  card.classList.remove('opacity-0', 'scale-95');
-  card.classList.add('opacity-100', 'scale-100');
-});
