da5e37425dc0dc847e5b4ff87730e149a40c47a6
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-01-20 17:50:38 +0100

    019

--- 019 SPACEX/src/main.js
+++ 019 SPACEX/src/main.js
@@ -1,9 +1,2 @@
 import { gsap } from "gsap";
-
-document.addEventListener("DOMContentLoaded", () => {
-  gsap.fromTo(
-    "#card",
-    { scale: 0.9, opacity: 0 },
-    { scale: 1, opacity: 1, duration: 1.5, ease: "power3.out" }
-  );
-});
+import 'animate.css';
