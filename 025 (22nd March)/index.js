2be272b4c24b33f8c2ee3da6d2c0e50c82a7f917
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-03-22 15:10:40 +0100

    BUILD: Addede JS and HTML

--- 025 (22nd March)/index.js
+++ 025 (22nd March)/index.js
@@ -1,1 +1,35 @@
 import inquirer from "inquirer";
+import bodyParser from "body-parser";
+import qr from "qr-image";
+import express from "express";
+
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+var qrLocker = "";
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+function Locker(req, res, next) {
+    console.log(req.body);
+    qrLocker = req.body["file"] + req.body["expiration"];
+    next();
+}
+
+app.use(Locker);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/upload", (req, res) => {
+    res.send(req.body);
+});
+
+app.listen(port, () => {
+    console.log(`Server is on port ${port}`);
+});
