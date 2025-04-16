2132c677b46c7fa8ad5ab30f4e361995356a2730
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-04-16 17:39:33 +0100

    build: Added About page

--- art-gallery/src/App.jsx
+++ art-gallery/src/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './css/App.css'
 import NavBar from './pages/Nav.jsx'
 import Home from './pages/Home.jsx'
+import About from './pages/About.jsx';
 
 function App() {
 
   return (
     <Router>
     <NavBar />
-    <Home/>
     <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/about" element={<About/>} />
     </Routes>
   </Router>
   
