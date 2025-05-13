6a933f69cee83148d5e0e42526da85c990603a2f
Enijeshiku Eniola <eenijeshiku@gmail.com>
2025-05-13 15:28:19 +0100

    fix

--- contact-page/src/ContactForm.jsx
+++ contact-page/src/ContactForm.jsx
@@ -3,6 +3,22 @@ import React, { useState } from "react";
 const ContactForm = () => {
     const [formData, setFormData] = useState ({
         fullName: '',
-        email: ''
-    })
+        email: '',
+        service: '',
+        message: ''
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({ ...prev, [name] : value}));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Submitted:', formData);
+        alert('Sent!'); 
+        setFormData({
+            fullName: '',
+        })
+    }
 }
